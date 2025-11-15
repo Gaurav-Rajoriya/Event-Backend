b0ddfae5429d78ea8d2024cdfb720072b1c0be54
Gaurav Rajoriya <https://github.com/Gaurav-Rajoriya>
2025-11-15 22:49:05 +0530

    Updated again

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,3 +1,20 @@
+router.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 const Data = require("../models/dataModel");
--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const dataRoutes = require("./routes/dataRoutes");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 
 // Serve static folder (optional)
